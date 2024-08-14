c26f75651a2f202190cabc696474eb6080287fec
Nekotori <nekotori55@gmail.com>
2024-08-14 19:28:33 +0300

    vm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -78,6 +78,46 @@
   # BLUETOOTH
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+  
+  
+  #####################################################################################
+  # VM 
+  
+  virtualisation.vmVariant = {
+  # following configuration is added only when building VM with build-vm
+  	virtualisation = {
+    	memorySize = 4096; # Use 2048MiB memory.
+    	cores = 3;   
+    };
+    
+    virtualisation.qemu.options = [
+    "-device virtio-vga-gl"
+    "-display sdl,gl=on,show-cursor=off"
+     # Wire up pipewire audio
+    "-audiodev pipewire,id=audio0"
+    "-device intel-hda"
+    "-device hda-output,audiodev=audio0"
+  	];
+  };
+  
+  virtualisation.vmVariantWithBootLoader = {
+  # following configuration is added only when building VM with build-vm
+  	virtualisation = {
+    	memorySize =  4096; # Use 2048MiB memory.
+    	cores = 3;         
+  	};
+  	
+  	virtualisation.qemu.options = [
+    "-device virtio-vga-gl"
+    "-display sdl,gl=on,show-cursor=off"
+     # Wire up pipewire audio
+    "-audiodev pipewire,id=audio0"
+    "-device intel-hda"
+    "-device hda-output,audiodev=audio0"
+  	];
+  };
+	
+	###################################################################################
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
