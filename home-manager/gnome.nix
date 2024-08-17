6dd195596890603eabdb433fd93736d266439562
Nekotori <nekotori55@gmail.com>
2024-08-17 18:47:48 +0300

    wallpaperh

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -8,11 +8,22 @@
     enable = true;
     #settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
-    settings."org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = with pkgs.gnomeExtensions; [
-        appindicator.extensionUuid
-      ];
+    settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          appindicator.extensionUuid
+        ];
+      };
+
+      "org/gnome/desktop/background" = 
+        let
+          bg = "https://w.wallhaven.cc/full/r2/wallhaven-r2dz6w.jpg";
+        in
+        {
+          picture-uri = "${bg}";
+          picture-uri-dark = "${bg}";
+        };
     };
   };
 }
