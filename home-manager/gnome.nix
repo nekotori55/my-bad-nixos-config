541851a3c06a6027c340cbba378a17064273cb66
Nekotori <nekotori55@gmail.com>
2024-08-17 18:13:56 +0300

    adaw

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -1,6 +1,18 @@
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    gnomeExtensions.appindicator
+  ];
+
   dconf = {
     enable = true;
     #settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+    settings."org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = with pkgs.gnomeExtensions; [
+        appindicator.extensionUuid
+      ];
+    };
   };
 }
