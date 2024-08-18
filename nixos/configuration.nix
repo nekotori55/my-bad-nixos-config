17709eb3418185216fcdb2aea105bcccb9afd007
Nekotori <nekotori55@gmail.com>
2024-08-18 17:02:11 +0300

    gc

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,6 +19,14 @@
 
   nix.settings.experimental-features = "nix-command flakes";
 
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than +15";
+  };
+
   # Bootloader.
   boot.loader = {
     grub = {
