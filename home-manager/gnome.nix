6d68e64c1aa61dcc6bdb4e2af58028346a761bd5
Nekotori <nekotori55@gmail.com>
2024-08-27 15:31:22 +0300

    gnome declarative keybinds

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -7,9 +7,8 @@
 
   dconf = {
     enable = true;
-    #settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
-    settings = {
+    settings = let keybind-base-path = "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"; in {
       "org/gnome/shell" = {
         disable-user-extensions = false;
         enabled-extensions = with pkgs.gnomeExtensions; [
@@ -18,7 +17,6 @@
         ];
       };
 
-      # 
       "org/gnome/mutter" = {
         check-alive-timeout = 60000;
       };
@@ -32,6 +30,36 @@
           picture-uri = "${bg}";
           picture-uri-dark = "${bg}";
         };
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/${keybind-base-path}/custom0/"
+          "/${keybind-base-path}/custom1/"
+          "/${keybind-base-path}/custom2/"
+          "/${keybind-base-path}/custom3/"
+        ];
+      };
+
+      "${keybind-base-path}/custom0" = {
+        binding = "<Super>w";
+        command = "firefox";
+        name = "Firefox";
+      };
+      "${keybind-base-path}/custom1" = {
+        binding = "<Super>t";
+        command = "kgx";
+        name = "Terminal";
+      };
+      "${keybind-base-path}/custom2" = {
+        binding = "<Super>e";
+        command = "nautilus";
+        name = "Explorer";
+      };
+      "${keybind-base-path}/custom3" = {
+        binding = "<Shift><Super>e";
+        command = "nautilus admin:/";
+        name = "Explorer (administrator mode)";
+      };
     };
   };
 }
