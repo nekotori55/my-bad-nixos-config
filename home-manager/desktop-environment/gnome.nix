8b10a63d276f334289b1c7af6c9daf3bbfd11e3b
Nekotori <nekotori55@gmail.com>
2024-09-04 23:15:00 +0300

    move wallpaper setting from gnome

--- home-manager/desktop-environment/gnome.nix
+++ home-manager/desktop-environment/gnome.nix
@@ -44,11 +44,6 @@
 
   home.sessionVariables.GTK_THEME = "Gruvbox-Dark";
 
-  home.file.".wallpaper.png".source = builtins.fetchurl {
-    url = "https://w.wallhaven.cc/full/3l/wallhaven-3lyrvy.png";
-    sha256 = "1angwl1knhjxv3hfr6dzrnm7xzkgxv6vbv8ny2bkmr9f9wk6mwd8";
-  };
-
   dconf = {
     enable = true;
 
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,6 +15,12 @@
     ./development/default.nix
   ];
 
+  # Wallpaper
+  home.file.".wallpaper.png".source = builtins.fetchurl {
+    url = "https://w.wallhaven.cc/full/3l/wallhaven-3lyrvy.png";
+    sha256 = "1angwl1knhjxv3hfr6dzrnm7xzkgxv6vbv8ny2bkmr9f9wk6mwd8";
+  };
+
   home.packages = with pkgs; [
     neofetch
     cowsay
