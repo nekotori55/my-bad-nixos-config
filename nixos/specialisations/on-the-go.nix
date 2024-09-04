6e89f8698975f1a65a49d2fcaf14f56f9ddb98fe
Nekotori <nekotori55@gmail.com>
2024-09-04 22:54:55 +0300

    disable nvidia dgpu on-the-go

--- nixos/specialisations/on-the-go.nix
+++ nixos/specialisations/on-the-go.nix
@@ -9,14 +9,14 @@
 
       system.nixos.tags = [ "on-the-go" ];
       hardware = {
-        nvidia = {
-          prime.offload.enable = lib.mkForce true;
-          prime.offload.enableOffloadCmd = lib.mkForce true;
-          prime.sync.enable = lib.mkForce false;
+        # nvidia = {
+        #   prime.offload.enable = lib.mkForce true;
+        #   prime.offload.enableOffloadCmd = lib.mkForce true;
+        #   prime.sync.enable = lib.mkForce false;
 
-          powerManagement.enable = lib.mkForce true; # disable if artefacts
-          powerManagement.finegrained = lib.mkForce true; # works on turing or newer (should check)
-        };
+        #   powerManagement.enable = lib.mkForce true; # disable if artefacts
+        #   powerManagement.finegrained = lib.mkForce true; # works on turing or newer (should check)
+        # };
 
         bluetooth.powerOnBoot = lib.mkForce false;
       };
@@ -25,6 +25,31 @@
       powerManagement.cpuFreqGovernor = "powersave";
       powerManagement.enable = true;
       powerManagement.powertop.enable = true;
+
+      # disable nvidia 
+
+      boot.extraModprobeConfig = lib.mkDefault ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+
+      services.udev.extraRules = lib.mkDefault ''
+        # Remove NVIDIA USB xHCI Host Controller devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+
+        # Remove NVIDIA USB Type-C UCSI devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+
+        # Remove NVIDIA Audio devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+
+        # Remove NVIDIA VGA/3D controller devices
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+      '';
+      boot.blacklistedKernelModules = lib.mkDefault [
+        "nouveau"
+        "nvidia"
+      ];
     };
   };
 }
