ea3efb085eee50caa8bf905352712c6bd42081fb
Nekotori <nekotori55@gmail.com>
2024-09-04 20:30:02 +0300

    tweak pipewire settings to fix audio glitches on high cpu load

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,6 +87,18 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    extraConfig.pipewire = {
+      context.properties = {
+        default.clock.rate = 48000;
+        #defautlt.allowed-rates = [ 192000 48000 44100 ];
+        defautlt.allowed-rates = [ 48000 ];
+        default.clock.quantum = 2048;
+        default.clock.min-quantum = 1024;
+        #default.clock.max-quantum = 32;
+      };
+    };
+
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
     wireplumber.extraConfig = {
