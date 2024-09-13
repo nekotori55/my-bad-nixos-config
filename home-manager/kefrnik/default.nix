10fa8e045d854b1240609d40fca5d698b9c235c1
Nekotori <nekotori55@gmail.com>
2024-09-13 21:22:57 +0300

    changed wallpaper

--- home-manager/kefrnik/default.nix
+++ home-manager/kefrnik/default.nix
@@ -11,8 +11,8 @@
 
   # Wallpaper
   home.file.".wallpaper.png".source = builtins.fetchurl {
-    url = "https://w.wallhaven.cc/full/3l/wallhaven-3lyrvy.png";
-    sha256 = "1angwl1knhjxv3hfr6dzrnm7xzkgxv6vbv8ny2bkmr9f9wk6mwd8";
+    url = "https://w.wallhaven.cc/full/qz/wallhaven-qz8pqd.png";
+    sha256 = "sha256:03cfv801yxmpz6kfd5a4cmcxv3xx375bd394295f51ci2zzah6v4";
   };
 
   programs.git = {
