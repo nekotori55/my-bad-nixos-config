10deefe2fc812614bd9bc553a3f6bb247cafad35
Nekotori <nekotori55@gmail.com>
2024-09-19 13:01:57 +0300

    Change locale settings

--- nixos/hosts/laptop-hp/configuration.nix
+++ nixos/hosts/laptop-hp/configuration.nix
@@ -63,21 +63,21 @@
   networking.hostName = "nixos-xx";
 
   # Set your time zone.
-  time.timeZone = "Europe/Moscow";
+  time.timeZone = "Europe/Istanbul";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
 
   hardware.bluetooth = {
