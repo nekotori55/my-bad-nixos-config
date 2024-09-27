d520ca8f59740511d531aeb73723c2be7e7acb51
Nekotori <nekotori55@gmail.com>
2024-09-27 15:30:21 +0300

    update system

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/master";
