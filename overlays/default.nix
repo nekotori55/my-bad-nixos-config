b2bd62a7b2e7f84e69e0be817ade24fb7cf7d402
Nekotori <nekotori55@gmail.com>
2024-11-16 15:56:41 +0300

    overlays | remove discord overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,9 +3,6 @@
   additions = final: _prev: import ../pkgs final.pkgs;
 
   modifications = final: prev: {
-    discord = prev.discord.override {
-      withOpenASAR = true;
-      withVencord = true;
-    };
+
   };
 }
