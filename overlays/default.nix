cf5d88ca7754dd9caa34980cc3e6c9f723a5d687
Nekotori <nekotori55@gmail.com>
2024-11-17 16:47:51 +0300

    overlays | replace vesktop icon with the discord one

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,3 @@
-{ ... }:
 {
   ...
 }:
@@ -6,6 +5,13 @@ let
   additions = final: _prev: import ../pkgs final.pkgs;
 
   modifications = final: prev: {
+    # Replace vesktop icon with discord one
+    vesktop = prev.vesktop.overrideAttrs (e: {
+      desktopItems = (builtins.elemAt e.desktopItems 0).override (d: {
+        # Requires iconpack with discord icon or discord installed
+        icon = "discord";
+      });
+    });
   };
 in
 {
