1d8e301ff1c600cb62cdd69df729e35fb21a35be
Nekotori <nekotori55@gmail.com>
2024-11-27 17:23:41 +0300

    nixos | change default monospace font to Hack Nerd Mono

--- nixos/common/general.nix
+++ nixos/common/general.nix
@@ -13,14 +13,26 @@
   '';
 
   # Fonts
-  fonts.packages = with pkgs; [
-    fira-code
-    fira-code-symbols
-    corefonts
-    vistafonts
-    noto-fonts-cjk-sans
-  ];
-  fonts.enableDefaultPackages = true;
+  fonts = {
+    packages = with pkgs; [
+      fira-code
+      fira-code-symbols
+      corefonts
+      vistafonts
+      noto-fonts-cjk-sans
+      nerdfonts
+    ];
 
+    fontDir.enable = true;
+
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Hack Nerd Mono" ];
+      };
+    };
+
+    enableDefaultPackages = true;
+
+  };
   programs.nh.enable = true;
 }
